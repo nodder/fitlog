7f702e30c7f22da173977b6839625d79ac60ca44
nodder <nodder001@163.com>
2021-04-26 18:41:11 +0800

    1

---
+++ src/main/java/name/cdd/product/fitlog/TestDemo.java
@@ -0,0 +1,12 @@
+package name.cdd.product.fitlog;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("ssssss");
+    }
+}
